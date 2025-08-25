8da7fc37d8c7816a79f397a0aa5a7719932d033c
Nazar Koval <n.kovaldev@gmail.com>
2025-08-25 23:34:48 +0300

    setup default configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { ConfigService } from "@nestjs/config"
+import * as cookieParser from "cookie-parser"
+import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule)
-   await app.listen(process.env.PORT ?? 3000)
+
+   const config = app.get(ConfigService)
+
+   app.use(cookieParser(config.getOrThrow<string>("COOKIES_SECRET")))
+
+   app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+   app.enableCors({
+      origin: config.getOrThrow<string>("ALLOWED_ORIGIN"),
+      credentials: true,
+      exposedHeaders: ["Set-Cookie"],
+   })
+
+   await app.listen(config.getOrThrow<number>("APPLICATION_PORT") ?? 3000)
 }
+
 bootstrap()

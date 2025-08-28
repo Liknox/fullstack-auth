dc5e65dfcc27cc246abb7794997f1ad4fd900c0b
Nazar Koval <n.kovaldev@gmail.com>
2025-08-28 04:07:06 +0300

    fix: linter rules and adjust pkg versions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,7 @@ export default tseslint.config(
          "@typescript-eslint/restrict-template-expressions": "off",
          "@typescript-eslint/no-unsafe-call": "off",
          "@typescript-eslint/no-unsafe-assignment": "off",
+         "@typescript-eslint/no-unused-vars": "off"
       },
    }
 )

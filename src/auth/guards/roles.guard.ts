a040c3a46b10e65475b8f35b533521a00ff240ff
Nazar Koval <n.kovaldev@gmail.com>
2025-08-31 02:28:18 +0300

    feat: create roles guard and decorator

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,35 @@
+import {
+   CanActivate,
+   ExecutionContext,
+   ForbiddenException,
+   Injectable,
+} from "@nestjs/common"
+import { Reflector } from "@nestjs/core"
+import { ROLES_KEY } from "../decorators/roles.decorator"
+import { UserRole } from "@prisma/__generated__"
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+   public constructor(private readonly reflector: Reflector) {}
+
+   public canActivate(context: ExecutionContext): boolean {
+      const roles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+         context.getHandler(),
+         context.getClass(),
+      ])
+
+      const request = context.switchToHttp().getRequest()
+
+      if (!roles) {
+         return true
+      }
+
+      if (!roles.includes(request.user.role)) {
+         throw new ForbiddenException(
+            "Access Denied. You don't have enough permissions."
+         )
+      }
+
+      return true
+   }
+}

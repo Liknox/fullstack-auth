a067c66c3085843bb23d4086c0710228f12258d9
Nazar Koval <n.kovaldev@gmail.com>
2025-08-31 02:32:14 +0300

    fix: add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,12 +28,15 @@ export default tseslint.config(
       rules: {
          "@typescript-eslint/no-explicit-any": "off",
          "@typescript-eslint/no-floating-promises": "off",
-         "@typescript-eslint/no-unsafe-argument": "warn",
          "prettier/prettier": "off",
          "@typescript-eslint/restrict-template-expressions": "off",
          "@typescript-eslint/no-unsafe-call": "off",
          "@typescript-eslint/no-unsafe-assignment": "off",
-         "@typescript-eslint/no-unused-vars": "off"
+         "@typescript-eslint/no-unused-vars": "off",
+         "@typescript-eslint/no-unnecessary-type-assertion": "off",
+         "@typescript-eslint/no-unsafe-member-access": "off",
+         "@typescript-eslint/no-unsafe-argument": "off",
+         "@typescript-eslint/no-unsafe-return": "off",
       },
    }
 )

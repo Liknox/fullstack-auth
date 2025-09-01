c132cf6b8bb618da24e459843263d7d316bcfedb
Nazar Koval <n.kovaldev@gmail.com>
2025-09-01 03:59:16 +0300

    api: integrate recaptcha in to auth module and protect reg and log routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
-import { Module } from "@nestjs/common"
+import { Logger, Module } from "@nestjs/common"
 import { AuthService } from "./auth.service"
 import { AuthController } from "./auth.controller"
 import { UserService } from "@/user/user.service"
 
+import { GoogleRecaptchaModule } from "@nestlab/google-recaptcha"
+import { recaptchaConfig } from "@/config/recaptcha.config"
+
 @Module({
+   imports: [GoogleRecaptchaModule.forRootAsync(recaptchaConfig)],
    controllers: [AuthController],
    providers: [AuthService, UserService],
 })

b5e9fb45d1b2a52ad1250deb04d7d017843cc6c6
Nazar Koval <n.kovaldev@gmail.com>
2025-09-03 04:46:05 +0300

    type: add types for provider

---
+++ src/auth/provider/services/types/user-info.types.ts
@@ -0,0 +1,10 @@
+export type TypeUserInfo = {
+   id: string
+   email: string
+   picture: string
+   name: string
+   access_token?: string | null
+   refresh_token?: string
+   expires_at?: number
+   provider: string
+}

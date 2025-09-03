15faa301d3328065f0606efa262809d71446d778
Nazar Koval <n.kovaldev@gmail.com>
2025-09-03 05:01:32 +0300

    feat: register Provider in auth module using providers config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,19 @@ import { UserService } from "@/user/user.service"
 
 import { GoogleRecaptchaModule } from "@nestlab/google-recaptcha"
 import { recaptchaConfig } from "@/config/recaptcha.config"
+import { ProviderModule } from "./provider/provider.module"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { getProvidersConfig } from "@/config/providers.config"
 
 @Module({
-   imports: [GoogleRecaptchaModule.forRootAsync(recaptchaConfig)],
+   imports: [
+      ProviderModule.registerAsync({
+         imports: [ConfigModule],
+         useFactory: getProvidersConfig,
+         inject: [ConfigService],
+      }),
+      GoogleRecaptchaModule.forRootAsync(recaptchaConfig),
+   ],
    controllers: [AuthController],
    providers: [AuthService, UserService],
 })

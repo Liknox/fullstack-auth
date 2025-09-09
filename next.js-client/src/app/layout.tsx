67f689d5b96761ce52c907331c1e3000f0b00718
Nazar Koval <n.kovaldev@gmail.com>
2025-09-09 07:23:11 +0300

    chore: add metadata to the layout component

--- next.js-client/src/app/layout.tsx
+++ next.js-client/src/app/layout.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+   title: {
+      absolute: "Fullstack Auth",
+      template: "%s | Authorization"
+   },
+   description:
+      "Fullstack Authorization using Node, Nest.js, Postgresql (db), Redis (sessions), Prisma (orm), Docker (compose), Oauth2 (google), 2FA (email confirmation), captcha (cloudflare). Frontend - Next.js (ssr), Tailwind (ui), Radix-ui, Zod (validation), React-hook-form."
+}
+
 export default function RootLayout({
    children
 }: Readonly<{ children: React.ReactNode }>) {

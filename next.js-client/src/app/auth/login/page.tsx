c4c58d408a53c1af1070e1448260bfc391df40b8
Nazar Koval <n.kovaldev@gmail.com>
2025-09-10 15:39:40 +0300

    chore: create templates for login and register pages

---
+++ next.js-client/src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+   title: "Log in Account",
+   description: "log in account"
+}
+
+export default function LoginPage() {
+   return <div>Log in Account</div>
+}
---
+++ next.js-client/src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+   title: "Create Account",
+   description: "create account"
+}
+
+export default function RegisterPage() {
+   return <div>Create Account</div>
+}

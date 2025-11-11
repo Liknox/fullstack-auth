b833bf49048b49edb32373fc2001547d7e3613e4
Nazar Koval <n.kovaldev@gmail.com>
2025-11-11 16:18:52 +0200

    only frontend

--- nest.js-server/src/auth/email-confirmation/dto/confirmation.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from "class-validator"
-
-export class ConfirmationDto {
-   @IsString({ message: "Token should be a string" })
-   @IsNotEmpty({ message: "Token is required" })
-   token: string
-}

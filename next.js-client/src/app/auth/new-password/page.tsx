7a18d0079975ee256af5a10f615a907ecda3a4fd
Nazar Koval <n.kovaldev@gmail.com>
2025-11-11 17:54:15 +0200

    fix: useSearchParams() error during build stage
    * fixed by wrapping useSearchParams() in react's Suspense

--- next.js-client/src/app/auth/new-password/page.tsx
+++ next.js-client/src/app/auth/new-password/page.tsx
@@ -1,11 +1,18 @@
 import { Metadata } from "next"
+import { Suspense } from "react"
 
 import { NewPasswordForm } from "@/features/auth/components"
+
+import { Loading } from "@/shared/components/ui"
 
 export const metadata: Metadata = {
    title: "New Password"
 }
 
 export default function NewPasswordPage() {
-   return <NewPasswordForm />
+   return (
+      <Suspense fallback={<Loading />}>
+         <NewPasswordForm />
+      </Suspense>
+   )
 }
--- next.js-client/src/app/auth/new-verification/page.tsx
+++ next.js-client/src/app/auth/new-verification/page.tsx
@@ -1,11 +1,18 @@
 import { Metadata } from "next"
+import { Suspense } from "react"
 
 import { NewVerificationForm } from "@/features/auth/components"
+
+import { Loading } from "@/shared/components/ui"
 
 export const metadata: Metadata = {
    title: "Email Verification"
 }
 
 export default function NewVerificationPage() {
-   return <NewVerificationForm />
+   return (
+      <Suspense fallback={<Loading />}>
+         <NewVerificationForm />
+      </Suspense>
+   )
 }
